97989a1d18ed641247a49478e6408fea15356772
18356015272 <827412ls>
2020-04-05 15:30:20 +0800

    目录划分

---
+++ src/views/index/StoreFoot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-base">
+    <router-view/>
+    <div class="common-footer-wrap">
+      <van-tabbar>
+        <van-tabbar-item><router-link :to="{ name: ''}" class='item'><i class='iconfont icon-homepage' /><span class='text'>首页</span></router-link></van-tabbar-item>
+        <van-tabbar-item><router-link :to="{ name: ''}" class='item'><i class='iconfont icon-homepage' /><span class='text'>品类</span></router-link></van-tabbar-item>
+        <van-tabbar-item><router-link :to="{ name: ''}" class='item'><i class='iconfont icon-homepage' /><span class='text'>优秀设计</span></router-link></van-tabbar-item>
+        <van-tabbar-item badge="5"><router-link :to="{ name: ''}" class='item'><i class='iconfont icon-homepage' /><span class='text'>购物车</span></router-link></van-tabbar-item>
+        <van-tabbar-item><router-link :to="{ name: ''}" class='item'><i class='iconfont icon-homepage' /><span class='text'>我的</span></router-link></van-tabbar-item>
+      </van-tabbar>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "StoreFoot"
+    }
+</script>
+
+<style lang="scss" scoped>
+  .item{
+    .iconfont {
+      display: block;
+      width:30px;
+      height: 30px;
+      line-height: 30px;
+      font-size:30px;
+      text-align: center;
+      margin: 0 auto;
+    }
+  }
+</style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -6,6 +6,7 @@
       <index-menu></index-menu>
       <index-brand></index-brand>
       <index-foot></index-foot>
+      <store-foot></store-foot>
     </div>
   </div>
 </template>
@@ -15,9 +16,11 @@ import IndexHead from "./IndexHead";
 import IndexBanner from "./IndexBanner";
 import IndexMenu from "./IndexMenu";
 import IndexBrand from "./IndexBrand"
-import IndexFoot from "./IndexFoot.vue";
+import IndexFoot from "./IndexFoot";
+import StoreFoot from "./StoreFoot";
 export default {
   components: {
+    StoreFoot,
     IndexFoot,
     IndexBrand,
     IndexMenu,
