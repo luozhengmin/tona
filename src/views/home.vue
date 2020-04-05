e950874e8bdcb9b17d62cae09957e8db6d119f48
18356015272 <827412ls>
2020-04-05 15:49:58 +0800

    目录划分

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <div class="header">
-
+      <index-head></index-head>
     </div>
     <div>
       <router-view />
     </div>
     <div class="footer">
-        
+      <store-foot></store-foot>
     </div>
   </div>
 </template>
 
 <script>
+import IndexHead from "./index/IndexHead.vue";
+import StoreFoot from "./index/StoreFoot.vue";
+
+
 export default {
+  components: {
+    StoreFoot,
+    IndexHead},
   name: "home"
 };
 </script>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,31 +1,25 @@
 <template>
   <div>
     <div class="container">
-      <index-head></index-head>
       <index-banner></index-banner>
       <index-menu></index-menu>
       <index-brand></index-brand>
       <index-foot></index-foot>
-      <store-foot></store-foot>
     </div>
   </div>
 </template>
 
 <script>
-import IndexHead from "./IndexHead";
 import IndexBanner from "./IndexBanner";
 import IndexMenu from "./IndexMenu";
 import IndexBrand from "./IndexBrand"
 import IndexFoot from "./IndexFoot";
-import StoreFoot from "./StoreFoot";
 export default {
   components: {
-    StoreFoot,
     IndexFoot,
     IndexBrand,
     IndexMenu,
     IndexBanner,
-    IndexHead
   }
 
 }
