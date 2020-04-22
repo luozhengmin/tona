f1f7130900f98649112a8e1ef7f5ef251a6d9edd
dongdongchen <dd18355193335@163.com>
2020-04-22 15:36:47 +0800

    品类列表

---
+++ src/api/GoodsClassApi.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+const baseUrl = '/api/Goodsclass'
+
+const GoodsClassApi = {}
+
+GoodsClassApi.list = (params) => {
+  return request({
+    url: `${baseUrl}/index`,
+    params: params,
+    method: 'post'
+  })
+}
+
+
+export default GoodsClassApi
