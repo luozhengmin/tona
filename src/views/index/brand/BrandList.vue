a82b27167787b19d5852edd1984c22b24f7895c3
dongdongchen <dd18355193335@163.com>
2020-05-08 14:17:12 +0800

    品牌馆列表

--- src/views/index/brand/BrandList.vue
+++ src/views/index/brand/BrandList.vue
@@ -3,11 +3,9 @@
     <div class="head fix">
       <div class="wrap fix">
         <div class="head-ss fix">
-          <van-icon name="arrow-left" @click="$router.go(-1)"/>
+          <van-icon name="arrow-left" @click="$router.go(-1)" />
         </div>
-        <div class="head-logo">
-          品牌馆
-        </div>
+        <div class="head-logo">品牌馆</div>
         <div class="menu-ico" v-bind:class="{active:isActive}" v-on:click="isActive=!isActive">
           <span></span>
         </div>
@@ -17,31 +15,31 @@
           <ul class="ab fix">
             <li>
               <router-link to="/BrandList-1">
-                <img src="../../../assets/image/nav05.png">
+                <img src="../../../assets/image/nav05.png" />
                 <span>品牌馆</span>
               </router-link>
             </li>
             <li>
               <router-link to="/Design">
-                <img src="../../../assets/image/nav04.png">
+                <img src="../../../assets/image/nav04.png" />
                 <span>优秀设计</span>
               </router-link>
             </li>
             <li>
               <router-link to="/GlobalStore">
-                <img src="../../../assets/image/nav03.png">
+                <img src="../../../assets/image/nav03.png" />
                 <span>全球门店</span>
               </router-link>
             </li>
             <li>
               <router-link to="/Investment">
-                <img src="../../../assets/image/nav02.png">
+                <img src="../../../assets/image/nav02.png" />
                 <span>招商加盟</span>
               </router-link>
             </li>
             <li>
               <router-link to="/index">
-                <img src="../../../assets/image/nav01.png">
+                <img src="../../../assets/image/nav01.png" />
                 <span>关于我们</span>
               </router-link>
             </li>
@@ -55,11 +53,13 @@
         <div class="classify ab fix">
           <div class="classify-tltle ab">
             <h2>品类</h2>
-            <a href="">全部</a>
+            <a @click="setCate(null)">全部</a>
           </div>
           <div class="classify-list">
             <swiper class="swiper" :options="swiperOption">
-              <swiper-slide v-for="item in items" :key="item.id"><a>{{item.value}}</a></swiper-slide>
+              <swiper-slide v-for="item in items" :key="item.id">
+                <a @click="setCate(item.id)">{{item.value}}</a>
+              </swiper-slide>
             </swiper>
           </div>
         </div>
@@ -67,24 +67,27 @@
         <div class="classify ab fix">
           <div class="classify-tltle ab">
             <h2>产地</h2>
-            <a href="">全部</a>
+            <a @click="setArea(null)">全部</a>
           </div>
           <div class="classify-list">
             <swiper class="swiper" :options="swiperOption">
-              <swiper-slide v-for="(item,index) in areaList" :key="index"><router-link to="/BrandList-1">{{item}}</router-link></swiper-slide>
+              <swiper-slide v-for="(item,index) in areaList" :key="index">
+                <a @click="setArea(item)">{{item}}</a>
+              </swiper-slide>
             </swiper>
           </div>
         </div>
-
       </div>
       <div class="sub-brand-b wrap">
         <div class="col" v-for="(item,index) in brandList" :key="index">
           <router-link :to="{name:'BrandChild',query:{id:item.id}}">
             <div class="b-pic">
-              <img :src="item.store_banner">
+              <img :src="item.store_banner" />
             </div>
             <div class="main ab fix">
-              <div class="h-pic"><img :src="item.store_avatar"></div>
+              <div class="h-pic">
+                <img :src="item.store_avatar" />
+              </div>
               <div class="infor">
                 <h2>{{item.store_name}}</h2>
                 <p>{{item.store_mainbusiness}}</p>
@@ -98,124 +101,161 @@
   </div>
 </template>
 <script>
-  import IndexBanner from "../IndexBanner.vue";
-  import IndexFoot from "../IndexFoot.vue";
-  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-  import 'swiper/css/swiper.css'
-  import StoreApi from "@/api/HomeStoreApi";
-  import GoodsClassApi from "@/api/GoodsClassApi";
-  import axios from "@/utils/request";
+import IndexBanner from "../IndexBanner.vue";
+import IndexFoot from "../IndexFoot.vue";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+import StoreApi from "@/api/HomeStoreApi";
+import GoodsClassApi from "@/api/GoodsClassApi";
+import axios from "@/utils/request";
 
-  export default {
-    name:'BrandList',
-    components: {
-      Swiper,
-      SwiperSlide,
-      IndexFoot,
-      IndexBanner
-    },
-    created() {
-
-      this.getBanners();
-      this.getArea(this.$route.query.catid);
-    },
-    data(){
-      return{
-        isActive: false,
-        banners: [],
-        swiperOption: {
-          slidesPerView: "auto",
-          spaceBetween:15,
-        },
-        items:[],
-        areaList:[],
-        brandList:[],
-        cate_id:'',
-        area_name:''
-      }
-    },
-    methods: {
-      getBanners() {
-        axios.post("/api/Index/getAppadList/ap_id/",{ap_id:24,}).then(res => {
-          let banners = res.result.ad_list;
-          this.banners = banners;
-        });
+export default {
+  name: "BrandList",
+  components: {
+    Swiper,
+    SwiperSlide,
+    IndexFoot,
+    IndexBanner
+  },
+  created() {
+    this.getBanners();
+    this.cate_id = this.$route.query.catid;
+    this.getArea(this.cate_id);
+    this.getBrandList();
+  },
+  data() {
+    return {
+      isActive: false,
+      banners: [],
+      swiperOption: {
+        slidesPerView: "auto",
+        spaceBetween: 15
       },
-      getArea(catid){
-        let params = {
-          cate_id: this.cate_id,
-          area_name:this.area_name
-        };
-
-        GoodsClassApi.list(params).then(res => {
-
-          let newData = res.result.class_list
-          for (let i = 0; i < 4; i++) {
-            if(newData[i]['id'] == catid){
-              this.items = newData[i]['children']
-            }
+      items: [],
+      areaList: [],
+      brandList: [],
+      cate_id: null,
+      area_name: null
+    };
+  },
+  methods: {
+    getBanners() {
+      axios.post("/api/Index/getAppadList/ap_id/", { ap_id: 24 }).then(res => {
+        let banners = res.result.ad_list;
+        this.banners = banners;
+      });
+    },
+    getArea(catid) {
+      GoodsClassApi.list().then(res => {
+        let newData = res.result.class_list;
+        for (let i = 0; i < 4; i++) {
+          if (newData[i]["id"] == this.cate_id) {
+            this.items = newData[i]["children"];
           }
+        }
+      });
 
-        });
-
-        StoreApi.storeArea(params).then(res => {
-          this.areaList = res.result;
-          console.log(this.areaList)
-        }).catch((error) => {
-          console.log("error")
-        });
-
-        StoreApi.storeBrand().then(res => {
-          this.brandList = res.result.store_list;
-        }).catch((error) => {
-          console.log("error")
-        });
-      },
+      StoreApi.storeArea().then(res => {
+        this.areaList = res.result;
+      });
+    },
+    getBrandList() {
+      let params = {
+        cate_id: this.cate_id,
+        area_name: this.area_name
+      };
+      StoreApi.storeBrand(params).then(res => {
+        this.brandList = res.result.store_list;
+      });
+    },
+    setCate(cateId) {
+      this.cate_id = cateId;
+      this.getBrandList();
+    },
+    setArea(area) {
+      this.area_name = area;
+      this.getBrandList();
     }
   }
+};
 </script>
 <style lang="scss" scoped>
-  .container{background-color:#f7f7f7;}
-  .sub-brand-t{
-    margin:0 0 12px 0;
-    background-color:#fff;
-    padding:2px 0 2px 15px;
-    .classify{
-      align-items:center;
-      margin:12px 0;
-      .classify-tltle{
-        width:33.5%;
-        align-items:center;
-        h2{font-size:14px;color:#323232;}
-        a{display:block;padding:0 20px;background-color:#f4f4f4;margin:0 12px;font-size:14px;color:#323232;border-radius:25px;height:32px;line-height:32px;}
-        a:hover{background-color:red;color:#fff;}
+.container {
+  background-color: #f7f7f7;
+}
+.sub-brand-t {
+  margin: 0 0 12px 0;
+  background-color: #fff;
+  padding: 2px 0 2px 15px;
+  .classify {
+    align-items: center;
+    margin: 12px 0;
+    .classify-tltle {
+      width: 33.5%;
+      align-items: center;
+      h2 {
+        font-size: 14px;
+        color: #323232;
       }
-      .classify-list{
-        position:relative;overflow:hidden;width:66.5%;
+      a {
+        display: block;
+        padding: 0 20px;
+        background-color: #f4f4f4;
+        margin: 0 12px;
+        font-size: 14px;
+        color: #323232;
+        border-radius: 25px;
+        height: 32px;
+        line-height: 32px;
+      }
+      a:hover {
+        background-color: red;
+        color: #fff;
       }
     }
+    .classify-list {
+      position: relative;
+      overflow: hidden;
+      width: 66.5%;
+    }
   }
-  .sub-brand-b{
-    .col{
-      width:100%;
-      position:relative;
-      margin-bottom:12px;
-      a{display:block;
-        .b-pic{text-align:center;
-          img{border-radius:2px;}
+}
+.sub-brand-b {
+  .col {
+    width: 100%;
+    position: relative;
+    margin-bottom: 12px;
+    a {
+      display: block;
+      .b-pic {
+        text-align: center;
+        img {
+          border-radius: 2px;
         }
-        .main{
-          align-items:center;
-          background-color:#fff;
-          border:solid 1px #f2f2f2;
-          .h-pic{width:45px;height:45px;margin:0 12px;}
-          .infor{
-            padding:18px 10px 18px 0;
-            h2{font-size:15px;color:#323232;padding-bottom:3px;}
-            p{color:#888;font-size:14px;}
+      }
+      .main {
+        align-items: center;
+        background-color: #fff;
+        border: solid 1px #f2f2f2;
+        .h-pic {
+          width: 45px;
+          height: 45px;
+          margin: 0 12px;
+        }
+        .infor {
+          padding: 18px 10px 18px 0;
+          h2 {
+            font-size: 15px;
+            color: #323232;
+            padding-bottom: 3px;
+          }
+          p {
+            color: #888;
+            font-size: 14px;
           }
         }
       }
     }
   }
+}
 </style>
