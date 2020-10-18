0c5ba9d0ecb713ecae26ed33399207eb7944d929
dongdongchen <dd18355193335@163.com>
2020-10-18 17:38:29 +0800

    优化

--- src/api/memberInfo.js
+++ src/api/memberInfo.js
@@ -68,6 +68,58 @@ export const updatePassword =
     })
   }
 
+// 发送验证码
+export const sendCode =
+  (params) => {
+    return request({
+      url: `/api/Memberaccount/check_auth_code`,
+      headers: {
+        'X-DS-KEY': $cookies.get('token'),//设置请求头请求格式为JSON
+      },
+      data: params,
+      method: 'POST'
+    })
+  }
+
+// 验证手机号
+export const validMobile =
+  (params) => {
+    return request({
+      url: `/api/Memberaccount/get_mobile_info`,
+      headers: {
+        'X-DS-KEY': $cookies.get('token'),//设置请求头请求格式为JSON
+      },
+      data: params,
+      method: 'POST'
+    })
+  }
+
+// 修改手机号发送验证码
+export const sendSmsCode =
+  (params) => {
+    return request({
+      url: `/api/Memberaccount/bind_mobile_step1`,
+      headers: {
+        'X-DS-KEY': $cookies.get('token'),//设置请求头请求格式为JSON
+      },
+      data: params,
+      method: 'POST'
+    })
+  }
+
+// 验证验证码并保存信息
+export const saveMobile =
+  (params) => {
+    return request({
+      url: `/api/Memberaccount/bind_mobile_step2`,
+      headers: {
+        'X-DS-KEY': $cookies.get('token'),//设置请求头请求格式为JSON
+      },
+      data: params,
+      method: 'POST'
+    })
+  }
+
 
 // 获取用户地址列表
 export const getMemberAddressList =
--- src/views/index/StoreFoot.vue
+++ src/views/index/StoreFoot.vue
@@ -1,36 +1,45 @@
 <template>
   <div class="home-base">
     <div class="common-footer-wrap">
-      <van-tabbar active-color="#f12c20" inactive-color="#888" v-model="active">
+      <van-tabbar
+        active-color="#f12c20"
+        inactive-color="#888"
+        v-model="active"
+        @change="onChange"
+      >
         <van-tabbar-item class="foot-item" to="/index">
           <template #icon="props">
             <img :src="props.active ? icon.active : icon.inactive" />
           </template>
-          <span class='text'>首页</span>
+          <span class="text">首页</span>
         </van-tabbar-item>
         <van-tabbar-item class="foot-item" to="/ProductList">
           <template #icon="props">
             <img :src="props.active ? icon01.active : icon01.inactive" />
           </template>
-          <span class='text'>品类</span>
+          <span class="text">品类</span>
         </van-tabbar-item>
         <van-tabbar-item class="foot-item" to="/Design">
           <template #icon="props">
             <img :src="props.active ? icon02.active : icon02.inactive" />
           </template>
-          <span class='text'>优秀设计</span>
+          <span class="text">优秀设计</span>
         </van-tabbar-item>
-        <van-tabbar-item :badge="carNum==0?'':carNum" class="foot-item" to="/cart">
+        <van-tabbar-item
+          :badge="carNum == 0 ? '' : carNum"
+          class="foot-item"
+          to="/cart"
+        >
           <template #icon="props">
             <img :src="props.active ? icon03.active : icon03.inactive" />
           </template>
-          <span class='text'>购物车</span>
+          <span class="text">购物车</span>
         </van-tabbar-item>
         <van-tabbar-item class="foot-item" to="/person">
           <template #icon="props">
             <img :src="props.active ? icon04.active : icon04.inactive" />
           </template>
-          <span class='text'>我的</span>
+          <span class="text">我的</span>
         </van-tabbar-item>
       </van-tabbar>
     </div>
@@ -38,45 +47,54 @@
 </template>
 
 <script>
-    import { cartNumGet } from '../../api/memberCart'
-    export default {
-        name: "StoreFoot",
-        data(){
-          return {
-            carNum : '',
-            active: 0,
-            icon: {
-              active: '../../../static/icon/ficon05-h.png',
-              inactive: '../../../static/icon/ficon05.png',
-            },
-            icon01: {
-              active: '../../../static/icon/ficon04-h.png',
-              inactive: '../../../static/icon/ficon04.png',
-            },
-            icon02: {
-              active: '../../../static/icon/ficon03-h.png',
-              inactive: '../../../static/icon/ficon03.png',
-            },
-            icon03: {
-              active: '../../../static/icon/ficon02-h.png',
-              inactive: '../../../static/icon/ficon02.png',
-            },
-            icon04: {
-              active: '../../../static/icon/ficon01-h.png',
-              inactive: '../../../static/icon/ficon01.png',
-            },
-          }
-        },
-        created(){
-          cartNumGet().then(
-            response => {
-              console.log(response)
-              this.carNum = response.result.cart_count
-
-            },
-            error => {}
-          )
-        }
+import { cartNumGet } from "../../api/memberCart";
+export default {
+  name: "StoreFoot",
+  data() {
+    return {
+      carNum: "",
+      active: 0,
+      icon: {
+        active: "../../../static/icon/ficon05-h.png",
+        inactive: "../../../static/icon/ficon05.png",
+      },
+      icon01: {
+        active: "../../../static/icon/ficon04-h.png",
+        inactive: "../../../static/icon/ficon04.png",
+      },
+      icon02: {
+        active: "../../../static/icon/ficon03-h.png",
+        inactive: "../../../static/icon/ficon03.png",
+      },
+      icon03: {
+        active: "../../../static/icon/ficon02-h.png",
+        inactive: "../../../static/icon/ficon02.png",
+      },
+      icon04: {
+        active: "../../../static/icon/ficon01-h.png",
+        inactive: "../../../static/icon/ficon01.png",
+      },
+    };
+  },
+  created() {
+    var lastTabIndex = $cookies.get("lastTabIndex");
+    if (!lastTabIndex) {
+      lastTabIndex = 0;
     }
+    this.active = Number(lastTabIndex);
+    cartNumGet().then(
+      (response) => {
+        console.log(response);
+        this.carNum = response.result.cart_count;
+      },
+      (error) => {}
+    );
+  },
+  methods: {
+    onChange(index) {
+      $cookies.set("lastTabIndex", index);
+    },
+  },
+};
 </script>
 
